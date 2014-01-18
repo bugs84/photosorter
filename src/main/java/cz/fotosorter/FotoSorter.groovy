cdd5eb03e8f902ae386766b61643fe5e1931240f
Honza <Honza@Centurion>
2014-01-18 16:29:44 +0100

    Reading date from exif

--- src/main/java/cz/fotosorter/FotoSorter.groovy
+++ src/main/java/cz/fotosorter/FotoSorter.groovy
@@ -1,13 +1,24 @@
 package cz.fotosorter
 
+import com.drew.imaging.ImageMetadataReader
+import com.drew.metadata.exif.ExifSubIFDDirectory
+
 def source = new File("c:\\foto\\AANikkonTransfer\\004_DELETE_FotoSorter_SOURCE")
 def destination = new File("c:\\foto\\AANikkonTransfer\\004_DELETE_FotoSorterOUTPUT")
 
 source.eachFile {
-    processFile()
+    processFile(it)
+
 }
 
-private void processFile() {
-    println "read and move"
+private void processFile(File image) {
+    def metadata = ImageMetadataReader.readMetadata(image);
+    ExifSubIFDDirectory directory = metadata.getDirectory(ExifSubIFDDirectory.class);
+    Date date = directory.getDate(ExifSubIFDDirectory.TAG_DATETIME_ORIGINAL);
+
+    println "$date - $image.name"
+
+
+
 }
 
