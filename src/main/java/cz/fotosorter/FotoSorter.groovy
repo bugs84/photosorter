b0cd86ccd7eeade7ca37f1df0c55efba65da13bb
Honza <Honza@Centurion..>
2014-01-18 16:53:16 +0100

    Obtain destination folder name

--- src/main/java/cz/fotosorter/FotoSorter.groovy
+++ src/main/java/cz/fotosorter/FotoSorter.groovy
@@ -12,13 +12,29 @@ source.eachFile {
 }
 
 private void processFile(File image) {
+    Date date = getImageDate(image)
+
+    String folder = getFolder(date)
+
+    println "$folder - $date - $image.name"
+
+
+
+}
+
+private Date getImageDate(File image) {
     def metadata = ImageMetadataReader.readMetadata(image);
     ExifSubIFDDirectory directory = metadata.getDirectory(ExifSubIFDDirectory.class);
     Date date = directory.getDate(ExifSubIFDDirectory.TAG_DATETIME_ORIGINAL);
+    date
+}
 
-    println "$date - $image.name"
-
-
-
+private String getFolder(Date date) {
+    def c = Calendar.getInstance()
+    c.setTime(date)
+    def year = c.get(Calendar.YEAR)
+    def month = c.get(Calendar.MONTH) + 1
+    def day = c.get(Calendar.DAY_OF_MONTH)
+    String.format("%d_%02d_%02d", year, month, day)
 }
 
