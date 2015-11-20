3ed9b309919ae3adbf1026a67c90ec452309a7c4
bugs <bugs@DESKTOP-454PLEV..>
2015-11-20 12:09:54 +0100

    Crc for photos as string

---
+++ src/main/java/cz/fotosorter/util/PhotoCrc.groovy
@@ -0,0 +1,22 @@
+package cz.fotosorter.util
+
+import org.apache.commons.io.FileUtils
+
+class PhotoCrc {
+    private File file
+    private String crc
+
+    PhotoCrc(File file) {
+        this.file = file
+        doChecksum()
+    }
+
+    private void doChecksum() {
+        crc = FileUtils.checksumCRC32(file).toString()
+    }
+
+    String getCrc() {
+        crc
+    }
+
+}
