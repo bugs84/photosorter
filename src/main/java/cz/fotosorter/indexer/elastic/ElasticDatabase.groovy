77918d8b48145ef7b7a9313e3b678d9c6072f1ec
bugs <bugs@DESKTOP-454PLEV..>
2015-11-22 17:47:39 +0100

    Using logger instead of println

--- src/main/java/cz/fotosorter/indexer/elastic/ElasticDatabase.groovy
+++ src/main/java/cz/fotosorter/indexer/elastic/ElasticDatabase.groovy
@@ -13,8 +13,12 @@ import org.elasticsearch.action.index.IndexResponse
 import org.elasticsearch.client.Client
 import org.elasticsearch.node.Node
 import org.elasticsearch.node.NodeBuilder
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 
 class ElasticDatabase implements Database {
+
+    private static final Logger logger = LoggerFactory.getLogger(ElasticDatabase.class)
 
     private String databaseDirectory = "target/elastic-database"
 
@@ -53,10 +57,10 @@ class ElasticDatabase implements Database {
         //this is needed, because I am not able setup "ignore_unavailable" in get request
         IndicesExistsResponse existsResponse = client.admin().indices().prepareExists(indexName).execute().actionGet()
         if (!existsResponse.exists) {
-            println "Index doesn't exists - going to create new."
+            logger.info "Index doesn't exists - going to create new."
             createIndex()
         } else {
-            println "Ok - Index already exists."
+            logger.info "Ok - Index already exists."
         }
     }
 
@@ -115,19 +119,18 @@ class ElasticDatabase implements Database {
         IndexRequest indexRequest = new IndexRequest(indexName, type, photoInfo.crc);
         indexRequest.source(jsonWriter.writeValueAsString(photoInfo))
         IndexResponse response = this.client.index(indexRequest).actionGet();
-//        println response
 
         //Get document - whole
         GetRequestBuilder getRequestBuilder = client.prepareGet(indexName, type, photoInfo.crc);
         GetResponse getResponse = getRequestBuilder.execute().actionGet();
-        println "WHOLE object\n" + getResponse.getSourceAsString()
+        logger.info "WHOLE object\n" + getResponse.getSourceAsString()
 
         //Get document - one field
         GetRequestBuilder getRequestBuilder2 = client.prepareGet(indexName, type, photoInfo.crc);
         getRequestBuilder2.setFields("originalPath");
         GetResponse getResponse2 = getRequestBuilder2.execute().actionGet();
         String gettedField2 = getResponse2.getField("originalPath").getValue().toString();
-        println "One Field\n" + gettedField2 + "\n\n"
+        logger.info "One Field\n" + gettedField2 + "\n\n"
 
 
 
