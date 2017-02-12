e2637e73fd59850ec4d1f801f39688da277fd858
j.vondrous <j.vondrous@localhost>
2017-02-12 17:26:48 +0100

    PhotoSorter handle situation when destination file already exists.

--- src/test/groovy/cz/vondr/photosorter/FileNameFormatterTest.groovy
+++ src/test/groovy/cz/vondr/photosorter/FileNameFormatterTest.groovy
@@ -9,11 +9,37 @@ class FileNameFormatterTest {
 
     @Test
     void 'file name format test'() {
-        //1.2.2015 18:03:04
-        def date = new GregorianCalendar(2015, 0, 2, 18, 3, 4).time
+        def date = new GregorianCalendar(2015, 0, 2, 18, 3, 4).time //1.2.2015 18:03:04
 
         def name = fileNameFormatter.format(date, "IMG original name.jpg")
 
         assert name == "2015_01_02_18-03-04__IMG original name.jpg"
     }
+
+    @Test
+    void 'format put number to name'() {
+        def date = new GregorianCalendar(2015, 0, 2, 18, 3, 4).time //1.2.2015 18:03:04
+
+        def name = fileNameFormatter.format(date, "IMG original name.jpg", 789)
+
+        assert name == "2015_01_02_18-03-04__IMG original name_789.jpg"
+    }
+
+    @Test
+    void 'format do not put number in name when number is 1'() {
+        def date = new GregorianCalendar(2015, 0, 2, 18, 3, 4).time //1.2.2015 18:03:04
+
+        def name = fileNameFormatter.format(date, "IMG original name.jpg", 1)
+
+        assert name == "2015_01_02_18-03-04__IMG original name.jpg"
+    }
+
+    @Test
+        void 'name without dot works '() {
+            def date = new GregorianCalendar(2015, 0, 2, 18, 3, 4).time //1.2.2015 18:03:04
+
+            def name = fileNameFormatter.format(date, "name without dot", 789)
+
+            assert name == "2015_01_02_18-03-04__name without dot_789"
+        }
 }
--- src/test/groovy/cz/vondr/photosorter/PhotoSorterTest.groovy
+++ src/test/groovy/cz/vondr/photosorter/PhotoSorterTest.groovy
@@ -35,6 +35,23 @@ class PhotoSorterTest {
         assert new File(resultDir, expectedDirs[1]).list() == ["2015_07_17_18-09-11__IMG_5677.JPG"]
     }
 
+    @Test
+    void 'If destination directory already contains file - file with new name is created.'() {
+        //given
+        def (PhotoSorterSettings settings, File filesToSortDir, File resultDir) = setupPhotoSorterWithNoDatabase()
+
+        def existingFileInResult = new File(resultDir, "2015_05_08/2015_05_08_11-10-51__IMG_5537.JPG")
+        existingFileInResult.parentFile.mkdirs()
+        existingFileInResult.text = "dummy"
+
+        //when
+        new PhotoSorter(settings).sort()
+
+        //then
+        assert new File(resultDir, "2015_05_08").list() == ["2015_05_08_11-10-51__IMG_5537.JPG", "2015_05_08_11-10-51__IMG_5537_2.JPG", "2015_05_08_12-01-46__IMG_5545.JPG"]
+
+    }
+
     private List setupPhotoSorterWithNoDatabase() {
         File filesToSortDir = temporaryFolder.newFolder("files-to-sort")
         File resultDir = temporaryFolder.newFolder("result-dir")
