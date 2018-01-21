854776111d37cedefea208c3e7f61d02d2640de4
Jan Vondrous <Jan Vondrous@localhost>
2018-01-21 13:15:14 +0100

    Settings for new computer

--- src/main/groovy/cz/vondr/photosorter/MainRun.groovy
+++ src/main/groovy/cz/vondr/photosorter/MainRun.groovy
@@ -2,22 +2,27 @@ package cz.vondr.photosorter
 
 import cz.vondr.photosorter.settings.PhotoSorterSettings
 
-import static cz.vondr.photosorter.settings.FileOperation.NOTHING
+import static cz.vondr.photosorter.settings.FileOperation.*
 
 class MainRun {
     private void start() {
         def photoSorter = new PhotoSorter(new PhotoSorterSettings(
-        //        source: new File("c:/foto/AANikkonTransfer/toSort/"),
-                source: new File("c:/foto/AA_Mi2S_Transfer/Camera/"),
-        //        source: new File("c:/foto/AA_Canon_G12_Transfer/"),
-                destination: new File("c:/fotoDELETE"),
-                fileOperation: NOTHING,
-                databaseDirectory: new File("c:/foto/.photosorter/database/")
+                //        source: new File("c:/foto/AANikkonTransfer/toSort/"),
+                //        source: new File("d:/foto/AA_Mi2S_Transfer/Camera/"),
+//                source: new File("d:/foto/AANikkonTransfer/foto_Nikon_Besidka/102D3200/"),
+                source: new File("d:\\foto\\AA_G6_Spanelsko\\download_vse"),
+                //        source: new File("c:/foto/AA_Canon_G12_Transfer/"),
+                destination: new File("d:/foto"),
+                fileOperation: MOVE,
+
+                //TODO configure use database true/false  a ne ten muj vymysl s nulem
+                //TODO configure log directory
+                databaseDirectory: new File("d:/foto/.photosorter/database/")
         ))
         photoSorter.sort()
     }
 
-    public static void main(String[] args) {
+    static void main(String[] args) {
         MainRun app = new MainRun();
         app.start();
     }
