76827e08d9785977b5032a2f570e466fbaebaf2b
j.vondrous <j.vondrous@localhost>
2018-07-05 21:04:27 +0200

    SourceFileFinder basic test

---
+++ src/test/groovy/cz/vondr/photosorter/finder/SourceFileFinderTest.groovy
@@ -0,0 +1,45 @@
+package cz.vondr.photosorter.finder
+
+import cz.vondr.photosorter.settings.PhotoSorterSettings
+import org.junit.Rule
+import org.junit.Test
+import org.junit.rules.TemporaryFolder
+
+class SourceFileFinderTest {
+
+    @Rule
+    public TemporaryFolder temporaryFolder = new TemporaryFolder()
+
+    List<File> foundFiles = []
+
+    @Test
+    void testForEachFile() {
+        //given
+        File folder = temporaryFolder.newFolder("SourceFileFinderTest")
+        File pic1 = new File(folder, "picture1.jpg")
+        pic1.createNewFile()
+        File pic2 = new File(folder, "Picture 2.JPG")
+        pic2.createNewFile()
+        File pic3 = new File(folder, "Picture 3.JpG")
+        pic3.createNewFile()
+        File vid1 = new File(folder, "Video 1.mp4")
+        vid1.createNewFile()
+        File unknown = new File(folder, "Unknown file 1.mp3")
+        unknown.createNewFile()
+        File subFolder = new File(folder, "subfolder 1")
+        subFolder.mkdirs()
+        File subPic1 = new File(subFolder, "picture 1 in subfolder.jpg")
+        subPic1.createNewFile()
+
+        SourceFileFinder finder = new SourceFileFinder(new PhotoSorterSettings(source: folder))
+
+        //when
+        finder.forEachFile { file ->
+            foundFiles += file
+        }
+
+        //then
+        assert foundFiles as Set == [pic1, pic2, pic3] as Set
+
+    }
+}
