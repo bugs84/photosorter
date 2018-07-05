a1d53d36627604be6617a21dc653f0acf41c71d2
j.vondrous <j.vondrous@localhost>
2018-07-05 16:21:43 +0200

    AlltypesDateResolver. Survive if some resolver throw exception.

--- src/main/groovy/cz/vondr/photosorter/date_resolver/AllTypesDateResolver.groovy
+++ src/main/groovy/cz/vondr/photosorter/date_resolver/AllTypesDateResolver.groovy
@@ -1,25 +1,34 @@
 package cz.vondr.photosorter.date_resolver
 
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+
 class AllTypesDateResolver implements DateResolver {
+
+    private static final Logger logger = LoggerFactory.getLogger(AllTypesDateResolver.class)
 
     List<DateResolver> resolvers = [
             new JpgImageMetadataResolver()
     ]
 
     @Override
-
     DateResult resolveDate(File file) {
-        resolvers.findResult(
-                new DateResult(resolvedSuccessfully: false),
-                { resolver ->
-                    DateResult result = resolver.resolveDate(file)
-                    if (result.resolvedSuccessfully) {
-                        return result
-                    } else {
-                        return null
-                    }
-                }
-        )
+        for (DateResolver resolver : resolvers) {
+            DateResult result = tryResolveDate(resolver, file)
+            if (result.resolvedSuccessfully) {
+                return result
+            }
+        }
+        new DateResult(resolvedSuccessfully: false)
+    }
+
+    public DateResult tryResolveDate(DateResolver resolver, File file) {
+        try {
+            return resolver.resolveDate(file)
+        } catch (Exception ex) {
+            logger.warn("Resolver (${resolver.class.name}) threw exception", ex)
+            return new DateResult(resolvedSuccessfully: false)
+        }
     }
 
 
