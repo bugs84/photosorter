f25d5d6b999ed46eb9ed86aebc0351ed8679c267
Jan Vondrous <Jan Vondrous@localhost>
2018-11-10 11:22:50 +0100

    LoggerInitializer code cleanup

--- src/main/groovy/cz/vondr/photosorter/logger/LoggerInitializer.java
+++ src/main/groovy/cz/vondr/photosorter/logger/LoggerInitializer.java
@@ -13,6 +13,7 @@ import java.nio.file.StandardCopyOption;
 
 public class LoggerInitializer {
 
+    @SuppressWarnings("WeakerAccess")
     public static final String LOG_FILE_PROPERTY_KEY = "photosorter.logFile";
 
     private PhotoSorterSettings settings;
@@ -23,33 +24,33 @@ public class LoggerInitializer {
 
     public void setupLogger() {
         try {
-            setLogFileLocation();
-
-            Path tmpFile = Files.createTempFile("PhotoSorter_logger_config_", ".xml");
-            try (InputStream is = this.getClass().getResourceAsStream("/config/log4j2.xml")) {
-
-
-                tmpFile.toFile().deleteOnExit();
-                Files.copy(is, tmpFile, StandardCopyOption.REPLACE_EXISTING);
-            }
-
-
-            LoggerContext context = (LoggerContext) LogManager.getContext(false);
-            // this will force a reconfiguration
-            context.setConfigLocation(tmpFile.toUri());
-
-
-
-
-
-            context.getLogger("cz.vondr.photosorter.logger.Initializer").error("INITIALIZED");
-
-            LogManager.getLogger(this).error("XXXXXXX LogManager");
-
+            configure();
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
+    }
 
+    private void configure() throws IOException {
+        setLogFileLocation();
+
+        Path tmpFile = copyLoggerConfigToTmpFIle();
+        configureLoggerFromFile(tmpFile);
+
+        LogManager.getLogger(this).info("Logger has been initialized. Log file '" + getLogFile() + "'");
+    }
+
+    private void configureLoggerFromFile(Path tmpFile) {
+        LoggerContext context = (LoggerContext) LogManager.getContext(false);
+        context.setConfigLocation(tmpFile.toUri());
+    }
+
+    private Path copyLoggerConfigToTmpFIle() throws IOException {
+        Path tmpFile = Files.createTempFile("PhotoSorter_logger_config_", ".xml");
+        tmpFile.toFile().deleteOnExit();
+        try (InputStream is = this.getClass().getResourceAsStream("/config/log4j2.xml")) {
+            Files.copy(is, tmpFile, StandardCopyOption.REPLACE_EXISTING);
+        }
+        return tmpFile;
     }
 
     private void setLogFileLocation() throws IOException {
