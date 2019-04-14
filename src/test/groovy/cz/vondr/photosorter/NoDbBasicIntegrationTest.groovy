92ef33e7c0ae8d14690230e53d3561fbdf119180
Jan Vondrous <Jan Vondrous@localhost>
2019-04-14 19:51:50 +0200

    Removed debugging info from fixed test.

--- src/test/groovy/cz/vondr/photosorter/NoDbBasicIntegrationTest.groovy
+++ src/test/groovy/cz/vondr/photosorter/NoDbBasicIntegrationTest.groovy
@@ -4,8 +4,6 @@ import cz.vondr.photosorter.settings.PhotoSorterSettings
 import org.junit.Rule
 import org.junit.Test
 import org.junit.rules.TemporaryFolder
-import org.slf4j.Logger
-import org.slf4j.LoggerFactory
 import org.springframework.core.io.Resource
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver
 
@@ -15,7 +13,6 @@ import static cz.vondr.photosorter.settings.FileOperation.MOVE
 import static java.nio.file.Files.copy
 
 class NoDbBasicIntegrationTest {
-    private static final Logger logger = LoggerFactory.getLogger(PhotoSorter.class)
 
     @Rule
     public TemporaryFolder temporaryFolder = new TemporaryFolder()
@@ -25,7 +22,6 @@ class NoDbBasicIntegrationTest {
 
     @Test
     void 'basic'() {
-        logger.info("FAAAAAAAAAILING TESTTTTTTTTTTT")
         prepareSourceDirectory()
         createEmptyDestinationFolder()
 
@@ -35,16 +31,10 @@ class NoDbBasicIntegrationTest {
                 fileOperation: MOVE,
                 useDatabase: false
         ))
-        logger.info("II - Before sort")
         photoSorter.sort()
 
-        logger.info("II - source list")
         assert sourceFolder.list().size() == 0
-
-
-        String[] destList = destinationFolder.list()
-        logger.info("II - Dest List '$destList'")
-        assert destList as Set == [".photosorter", "2015_05_08", "2015_07_17"] as Set
+        assert destinationFolder.list() as Set == [".photosorter", "2015_05_08", "2015_07_17"] as Set
         assert new File(destinationFolder, "2015_05_08").list() as Set == ["2015_05_08_11-10-51__IMG_5537.JPG", "2015_05_08_12-01-46__IMG_5545.JPG"] as Set
         assert new File(destinationFolder, "2015_07_17").list() as Set == ["2015_07_17_18-09-11__IMG_5677.JPG"] as Set
         assert new File(destinationFolder, ".photosorter/logs/PhotoSorter.log").exists()
